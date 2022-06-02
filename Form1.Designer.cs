2cab332bacd8466cd60bb1abae12ac17c0c4afa6
antizombie35 <80841070+antizombie35@users.noreply.github.com>
2022-06-02 22:34:01 +0200

    IC

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace flowers
+{
+    partial class main
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winMoving = new System.Windows.Forms.PictureBox();
+            this.butExit = new System.Windows.Forms.Button();
+            this.butMinimalizeWin = new System.Windows.Forms.Button();
+            this.picBoxFlower = new System.Windows.Forms.PictureBox();
+            this.StartBut = new System.Windows.Forms.Button();
+            this.ans1But = new System.Windows.Forms.Button();
+            this.ans2But = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.winMoving)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxFlower)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // winMoving
+            // 
+            this.winMoving.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.winMoving.Location = new System.Drawing.Point(0, 0);
+            this.winMoving.Name = "winMoving";
+            this.winMoving.Size = new System.Drawing.Size(751, 24);
+            this.winMoving.TabIndex = 0;
+            this.winMoving.TabStop = false;
+            this.winMoving.MouseDown += new System.Windows.Forms.MouseEventHandler(this.winMoving_MouseDown);
+            // 
+            // butExit
+            // 
+            this.butExit.Location = new System.Drawing.Point(800, 8);
+            this.butExit.Margin = new System.Windows.Forms.Padding(2);
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(25, 25);
+            this.butExit.TabIndex = 1;
+            this.butExit.Text = "X";
+            this.butExit.UseVisualStyleBackColor = true;
+            this.butExit.Click += new System.EventHandler(this.butExit_Click);
+            // 
+            // butMinimalizeWin
+            // 
+            this.butMinimalizeWin.Location = new System.Drawing.Point(771, 8);
+            this.butMinimalizeWin.Margin = new System.Windows.Forms.Padding(2);
+            this.butMinimalizeWin.Name = "butMinimalizeWin";
+            this.butMinimalizeWin.Size = new System.Drawing.Size(25, 25);
+            this.butMinimalizeWin.TabIndex = 2;
+            this.butMinimalizeWin.Text = "_";
+            this.butMinimalizeWin.UseVisualStyleBackColor = true;
+            this.butMinimalizeWin.Click += new System.EventHandler(this.butMinimalizeWin_Click);
+            // 
+            // picBoxFlower
+            // 
+            this.picBoxFlower.Location = new System.Drawing.Point(12, 30);
+            this.picBoxFlower.Name = "picBoxFlower";
+            this.picBoxFlower.Size = new System.Drawing.Size(588, 309);
+            this.picBoxFlower.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxFlower.TabIndex = 3;
+            this.picBoxFlower.TabStop = false;
+            // 
+            // StartBut
+            // 
+            this.StartBut.Location = new System.Drawing.Point(618, 37);
+            this.StartBut.Margin = new System.Windows.Forms.Padding(2);
+            this.StartBut.Name = "StartBut";
+            this.StartBut.Size = new System.Drawing.Size(207, 25);
+            this.StartBut.TabIndex = 5;
+            this.StartBut.Text = "START";
+            this.StartBut.UseVisualStyleBackColor = true;
+            this.StartBut.Click += new System.EventHandler(this.StartBut_Click);
+            // 
+            // ans1But
+            // 
+            this.ans1But.Location = new System.Drawing.Point(145, 403);
+            this.ans1But.Margin = new System.Windows.Forms.Padding(2);
+            this.ans1But.Name = "ans1But";
+            this.ans1But.Size = new System.Drawing.Size(207, 25);
+            this.ans1But.TabIndex = 6;
+            this.ans1But.Text = "Ans1";
+            this.ans1But.UseVisualStyleBackColor = true;
+            this.ans1But.Click += new System.EventHandler(this.ans1But_Click);
+            // 
+            // ans2But
+            // 
+            this.ans2But.Location = new System.Drawing.Point(544, 403);
+            this.ans2But.Margin = new System.Windows.Forms.Padding(2);
+            this.ans2But.Name = "ans2But";
+            this.ans2But.Size = new System.Drawing.Size(207, 25);
+            this.ans2But.TabIndex = 7;
+            this.ans2But.Text = "Ans2";
+            this.ans2But.UseVisualStyleBackColor = true;
+            this.ans2But.Click += new System.EventHandler(this.ans2But_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.Controls.Add(this.ans2But);
+            this.Controls.Add(this.ans1But);
+            this.Controls.Add(this.StartBut);
+            this.Controls.Add(this.picBoxFlower);
+            this.Controls.Add(this.butMinimalizeWin);
+            this.Controls.Add(this.butExit);
+            this.Controls.Add(this.winMoving);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bio flowers";
+            this.Load += new System.EventHandler(this.mainLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.winMoving)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxFlower)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox winMoving;
+        private System.Windows.Forms.Button butExit;
+        private System.Windows.Forms.Button butMinimalizeWin;
+        private System.Windows.Forms.PictureBox picBoxFlower;
+        private System.Windows.Forms.Button StartBut;
+        private System.Windows.Forms.Button ans1But;
+        private System.Windows.Forms.Button ans2But;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Json.Net;
+using Newtonsoft.Json;
+
+namespace flowers
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        //public vars
+        public static string rAns = "";
+        public static Dictionary<string, string> dict;
+        public static Random r = new Random();
+
+        #region drag window part
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        private void butExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void winMoving_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void butMinimalizeWin_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        public void mainLoad(object sender, EventArgs e)
+        {
+
+            string json = @"{""key1"":""https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/YSvEcxy/videoblocks-corvus-corax-clarionensis-large-common-raven-head-closeup-black-raven-eyes-and-beak-macro-wild-bird-close-up-black-crow-head-close-up-black-crow-looking-straight-into-camera_rlugsa6m8_thumbnail-1080_01.png"",""key2"":""https://www.bylinkyprovsechny.cz/images/byliny-bylinky-fotografie-obrazek/kokoska-pastusi-tobolka-ucinky-na-zdravi-co-leci-pouziti-uzivani.jpg""}";
+            dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            Console.WriteLine("dict ready");
+        }
+
+        private void StartBut_Click(object sender, EventArgs e)
+        {
+
+            picBoxFlower.Load(dict.ElementAt(r.Next(0, dict.Count)).Value);
+        }
+
+        private void ans1But_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ans2But_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
